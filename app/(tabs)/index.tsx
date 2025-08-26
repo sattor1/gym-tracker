fef87dbad6cc450c0ac6ec41d1d76924a97e04b9
Sattor <60736981+sattor1@users.noreply.github.com>
2025-08-26 23:13:00 +0300

    add date picker

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,43 +1,28 @@
-import { Image } from 'expo-image';
+import RNDateTimePicker from '@react-native-community/datetimepicker';
 import { StyleSheet } from 'react-native';
-
-import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 
 export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }
-    >
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Gym tracker!!</ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+  return ( 
+    <ThemedView style={styles.titleContainer}>
+      <ThemedText type="title">Gym tracker!</ThemedText> 
+      <RNDateTimePicker value={new Date()} mode="date" locale="ru-RU" />
+    </ThemedView>
   );
 }
 
 const styles = StyleSheet.create({
   titleContainer: {
-    flexDirection: 'row',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
     alignItems: 'center',
     gap: 8,
+    height: '100%'
   },
   stepContainer: {
     gap: 8,
     marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
   },
 });
